461b660713b6a5b785266fb5cda985458282ee4d
Alex Kolesnyk <kolesnyk@adobe.com>
2019-08-27 13:41:33 -0500

    Update before_script.sh

--- dev/travis/before_script.sh
+++ dev/travis/before_script.sh
@@ -44,7 +44,7 @@ if [[ $TEST_SUITE = "functional" ]]; then
         sed -e "s?%ADOBE_STOCK_PRIVATE_KEY%?${ADOBE_STOCK_PRIVATE_KEY}?g" --in-place ./.env
         cd ../../..
         
-        ./vendor/magento/magento2-functional-testing-framework/bin/mftf generate:test --tests='${TEST_CONFIGURATION}'
+        ./vendor/magento/magento2-functional-testing-framework/bin/mftf generate:test --tests='{"tests":null,"suites":{"AdobeStockIntegrationSuite":[]}}'
 fi
 
 if [[ $TEST_SUITE = "api" ]]; then
